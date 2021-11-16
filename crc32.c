e55ce9e51e4df348a96dc38a13d165e95dd6818c
Leila F. Rahman <leilarahman@gmail.com>
2021-11-16 12:34:44 +0100

    Update crc32.c
    Add an unused variable. TICS github plugin testing.

--- crc32.c
+++ crc32.c
@@ -35,7 +35,7 @@ crc() routine below)). */
 unsigned long update_crc(unsigned long crc, unsigned char *buf, int len)
 {
 	unsigned long c = crc;
-	int n, i;
+	int n, i, k;
 
 	if (!crc_table_computed)
 		make_crc_table();
